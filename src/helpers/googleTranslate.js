2bc4c3ae8d07d892983702bb8fb35d8b7753e86c
Jimmy Lin <jimmylin0309@gmail.com>
2021-01-21 16:04:12 -0800

    add api and supportedLanguagesMap

--- src/helpers/googleTranslate.js
+++ src/helpers/googleTranslate.js
@@ -2,10 +2,9 @@
 
 import { key } from "../../keys/jimmy-translation.json";
 
-export const translate = (text, target) => {
-  const url = `https://translation.googleapis.com/language/translate/v2?q=${text}&target=${target}&key=${key}`;
-  const getTranslateTextPromise = new Promise((resolve, reject) => {
-    fetch(url, { method: 'POST' })
+const fetchData = (url, method, fieldName) => {
+  return new Promise((resolve, reject) => {
+    fetch(url, { method })
       .then(response => {
         if (response.status !== 200) {
           console.error(`Looks like there was a problem. Status Code: ${
@@ -13,10 +12,40 @@ export const translate = (text, target) => {
           return;
         }
         response.json().then(data => {
-          resolve(data.data.translations);
+          resolve(data.data[fieldName]);
         });
       })
       .catch(err => reject(err));
   });
-  return getTranslateTextPromise;
+};
+
+
+export const translate = (text, target) => {
+  const url = `https://translation.googleapis.com/language/translate/v2?q=${text}&target=${target}&key=${key}`;
+  return fetchData(url, 'POST', 'translations');
+};
+
+export const detectLanguage = text => {
+  const url = `https://translation.googleapis.com/language/translate/v2/detect?q=${text}&key=${key}`;
+  return fetchData(url, 'GET', 'detections');
+};
+
+export const getSupportedLanguages = target => {
+  const url = `https://translation.googleapis.com/language/translate/v2/languages?target=${target}&key=${key}`;
+  return fetchData(url, 'POST', 'languages');
+};
+
+export const supportedLanguagesMap = {
+  'German': 'de',
+  'English': 'en',
+  'Spanish': 'es',
+  'French': 'fr',
+  'Italian': 'it',
+  'Japanese': 'ja',
+  'Korean': 'ko',
+  'Dutch': 'nl',
+  'Portuguese': 'pt',
+  'Russian': 'ru',
+  'Chinese (Simplified)': 'zh_CN',
+  'Chinese (Traditional)': 'zh_TW'
 };
